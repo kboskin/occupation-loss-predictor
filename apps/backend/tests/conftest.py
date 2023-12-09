88d39696712391f5adb8999a6e71c90029fd449d
kbosk <kostyaboskin@gmail.com>
2023-12-09 16:55:01 +0200

    ci file updates

--- apps/backend/tests/conftest.py
+++ apps/backend/tests/conftest.py
@@ -9,3 +9,9 @@ from invasion import app
 async def client() -> TestClient:
     async with TestClient(app) as ac:
         yield ac
+
+
+def pytest_sessionfinish(session, exitstatus):
+    # no tests case
+    if exitstatus == 5:
+        session.exitstatus = 0  # Any arbitrary custom status you want to return```
