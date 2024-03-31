5ef6e5e721d032399df3cf42f00b36a04673d1f1
kbosk <kostyaboskin@gmail.com>
2024-03-31 20:13:16 +0300

    [Yearly aggregations]

--- apps/backend/invasion/losses/router.py
+++ apps/backend/invasion/losses/router.py
@@ -1,3 +1,4 @@
+import logging
 from typing import Annotated, Union, List
 
 from fastapi import APIRouter, Depends
@@ -19,18 +20,21 @@ losses_router.tags = ["losses"]
 
 @losses_router.get("/", response_model=LossesResponseModel)
 async def get_data_for_category(
-        commons: Annotated[PaginationParameters, Depends(pagination)],
-        categories: Annotated[Union[List[LossesProjectEnum], None], Depends(check_losses_category)],
-        session: Annotated[AsyncSession, Depends(get_session)]
+    commons: Annotated[PaginationParameters, Depends(pagination)],
+    categories: Annotated[Union[List[LossesProjectEnum], None], Depends(check_losses_category)],
+    session: Annotated[AsyncSession, Depends(get_session, use_cache=False)]
 ) -> LossesResponseModel:
     losses_with_categories: List[Loss] = []
     for category_item in categories:
         enum = LossesProjectEnum(category_item)
+        logging.debug(f"getting data for category {enum}")
         table = losses_enum_to_table_mapper(enum)
         data = await LossesService.get_data_for(session, commons.offset_from, commons.offset_to, table)
         last_data_record_data = data[-1].losses
 
+        logging.debug(f"getting data for category here")
         prediction_data = await ForecastService.get_prediction(session, enum, last_data_record_data)
+        logging.debug(f"getting data for category her2")
 
         loss = Loss(
             type=enum,
@@ -50,7 +54,35 @@ async def get_data_for_category(
 
 @losses_router.get("/yearly/aggregation", response_model=AggregationYearlyResponseModel)
 async def get_yearly_aggregations(
-        session: Annotated[AsyncSession, Depends(get_session)]
+    session: Annotated[AsyncSession, Depends(get_session, use_cache=False)]
+) -> AggregationYearlyResponseModel:
+    all_categories: List[List[AggregationDbLoss]] = [
+        await LossesService.get_aggregations(session, item) for item in
+        filter(lambda x: x != LossesProjectEnum.personnel, LossesProjectEnum.list())
+    ]
+    # Regroup the data
+    result = AggregationYearResult(children=[])
+    for category in all_categories:
+        for loss in category:
+            year_entry: AggregationYearTotal = next((item for item in result.children if item.name == loss.year), None)
+            if year_entry is None:
+                year_entry = AggregationYearTotal(name=loss.year, value=0, children=[])
+                result.children.append(year_entry)
+
+            year_entry.value += loss.total
+            year_entry.children.append(AggregationCategoryTotal(
+                name=loss.type,
+                value=loss.total
+            ))
+
+    result.children.sort(key=lambda x: x.name)
+
+    return AggregationYearlyResponseModel(data=result)
+
+
+@losses_router.get("/category/aggregation", response_model=AggregationYearlyResponseModel)
+async def get_yearly_aggregations(
+    session: Annotated[AsyncSession, Depends(get_session, use_cache=False)]
 ) -> AggregationYearlyResponseModel:
     all_categories: List[List[AggregationDbLoss]] = [
         await LossesService.get_aggregations(session, item) for item in
