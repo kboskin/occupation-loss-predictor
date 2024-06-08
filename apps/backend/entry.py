b730ea338557cdbc814a41bcd51f7e8eec8f11a7
kbosk <kostyaboskin@gmail.com>
2024-06-09 01:33:11 +0300

    [Fix flake comment]

--- apps/backend/entry.py
+++ apps/backend/entry.py
@@ -1,7 +1,6 @@
 import uvicorn
 
-#no qa
-from invasion import app
+from invasion import app  # noqa: F401
 
 if __name__ == '__main__':
     uvicorn.run("entry:app", host='127.0.0.1', port=8000, reload=True, workers=20)
