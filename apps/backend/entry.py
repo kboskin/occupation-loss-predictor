3135448857b47a9b3c5251105df6689f96c74466
kbosk <kostyaboskin@gmail.com>
2024-06-08 22:59:15 +0300

    [Add base support block] [Fix bug with writing forecast data into the database]

--- apps/backend/entry.py
+++ apps/backend/entry.py
@@ -1,4 +1,6 @@
 import uvicorn
 
+from invasion import app
+
 if __name__ == '__main__':
-    uvicorn.run("invasion:app", host='127.0.0.1', port=8000, reload=True, workers=20)
+    uvicorn.run(app, host='127.0.0.1', port=8000, reload=True, workers=20)
